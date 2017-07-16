ec30a36b1d08dd05e53cc05b824fe48703392586
Reynaldo <reynaldoeg_esparza@hotmail.com>
2017-07-16 12:33:03 -0500

    Implementing Complex Operations for Transactions and Buyers

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,10 @@ use Illuminate\Http\Request;
  * Buyers
  */
 Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+Route::resource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' => ['index']]);
+Route::resource('buyers.products', 'Buyer\BuyerProductController', ['only' => ['index']]);
+Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' => ['index']]);
+Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
 
 /**
  * Categories
@@ -41,6 +45,8 @@ Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show
  * Transactions
  */
 Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+Route::resource('transactions.categories', 'Transaction\TransactionCategoryController', ['only' => ['index']]);
+Route::resource('transactions.sellers', 'Transaction\TransactionSellerController', ['only' => ['index']]);
 
 /**
  * Users
